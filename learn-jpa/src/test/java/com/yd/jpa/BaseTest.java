124cc37810cc51ccbf8b736975dd4ceefe14175f
邹永东 <1406721322@qq.com>
2018-03-05 17:19:58 +0800

    + activiti demo init

--- learn-jpa/src/test/java/com/yd/jpa/BaseTest.java
+++ learn-jpa/src/test/java/com/yd/jpa/BaseTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/spring-base.xml"})
+@ContextConfiguration(locations = {"classpath:/spring.xml"})
 @TransactionConfiguration(defaultRollback=false)//spring-test 会自动回滚
 public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
 
