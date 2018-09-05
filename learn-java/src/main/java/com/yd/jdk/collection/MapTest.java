39b3f6a6306a4381efaf8b1c4dbc6e85cd7968af
Zeb-D <1406721322@qq.com>
2018-09-05 09:38:09 +0800

    +LruCache base on HashMap

--- learn-java/src/main/java/com/yd/jdk/collection/MapTest.java
+++ learn-java/src/main/java/com/yd/jdk/collection/MapTest.java
@@ -2,6 +2,7 @@ package com.yd.jdk.collection;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -12,10 +13,32 @@ import java.util.concurrent.ConcurrentSkipListMap;
  **/
 public class MapTest {
     Map map = new HashMap();
+
+    LinkedHashMap linkedHashMap = new LinkedHashMap();
+
     HashSet hashSet = new HashSet();
 
     ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
 
     ConcurrentSkipListMap concurrentSkipListMap = new ConcurrentSkipListMap();
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int total = 0;
+//        abc:
+//        for (int i = 0; i < 100; i++) {
+//            sum += i;
+//            total++;
+//            if (sum < 100) continue abc;
+//            else continue ;
+//        }
+        System.out.println(sum + "->" + total);
+
+        int a = 11, b = 1;
+        System.out.println(a ^ b);
+
+        boolean aa = true, bb = false;
+        System.out.println(aa && bb);
+    }
 
 }
