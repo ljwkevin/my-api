71fb60ff839260cd322a4cea4774556e884d78bd
Zeb-D <1406721322@qq.com>
2018-10-30 13:04:17 +0800

    +sort

--- learn-java/src/main/java/com/yd/algorithm/Sort.java
+++ learn-java/src/main/java/com/yd/algorithm/Sort.java
@@ -1,5 +1,7 @@
 package com.yd.algorithm;
 
+import java.util.stream.Stream;
+
 /**
  * 排序算法
  *
@@ -103,28 +105,80 @@ public class Sort {
      * @return
      */
     public static int[] shellSort(int[] arr) {
-        int i, j, gap;
-        // gap为步长，每次减为原来的一半。
-        for (gap = arr.length / 2; gap > 0; gap /= 2) {
-            // 共gap个组，对每一组都执行直接插入排序
-            for (i = 0; i < gap; i++) {
-                for (j = i + gap; j < arr.length; j += gap) {
-                    // 如果a[j] < a[j-gap]，则寻找a[j]位置，并将后面数据的位置都后移。
-                    if (arr[j] < arr[j - gap]) {
-                        int tmp = arr[j];
-                        int k = j - gap;
-                        while (k >= 0 && arr[k] > tmp) {
-                            arr[k + gap] = arr[k];
-                            k -= gap;
-                        }
-                        arr[k + gap] = tmp;
+        //增量gap，并逐步缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            //从第gap个元素，逐个对其所在组进行直接插入排序操作
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        //移动法
+                        arr[j] = arr[j - gap];
+                        j -= gap;
                     }
+                    arr[j] = temp;
                 }
             }
-
         }
         return arr;
     }
+
+
+    /**
+     * 堆排序
+     * <p>
+     * a.将无需序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆;
+     * <p>
+     * 　　b.将堆顶元素与末尾元素交换，将最大元素"沉"到数组末端;
+     * <p>
+     * 　　c.重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素，反复执行调整+交换步骤，直到整个序列有序。
+     * </p>
+     *
+     * @param arr
+     */
+    public static void HeapSort(int[] arr) {
+        //1.构建大顶堆
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+        int temp;
+        //2.调整堆结构+交换堆顶元素与末尾元素
+        for (int j = arr.length - 1; j > 0; j--) {
+            //swap(arr, 0, j);//将堆顶元素与末尾元素进行交换
+            temp = arr[0];
+            arr[0] = arr[j];
+            arr[j] = temp;
+
+            adjustHeap(arr, 0, j);//重新对堆进行调整
+        }
+
+    }
+
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     *
+     * @param arr
+     * @param i
+     * @param length
+     */
+    public static void adjustHeap(int[] arr, int i, int length) {
+        int temp = arr[i];//先取出当前元素i
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {//从i结点的左子结点开始，也就是2i+1处开始
+            if (k + 1 < length && arr[k] < arr[k + 1]) {//如果左子结点小于右子结点，k指向右子结点
+                k++;
+            }
+            if (arr[k] > temp) {//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;//将temp值放到最终的位置
+    }
+
 
     /**
      * 归并排序
@@ -140,8 +194,92 @@ public class Sort {
      * @return
      */
     public static int[] mergeSort(int[] arr) {
-
+        int[] temp = new int[arr.length];//在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        mergeSort(arr, 0, arr.length - 1, temp);
         return arr;
+    }
+
+    /**
+     * 递归式将无序数组拆分成最小有序数组
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param temp
+     */
+    private static void mergeSort(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid, temp);//左边归并排序，使得左子序列有序
+            mergeSort(arr, mid + 1, right, temp);//右边归并排序，使得右子序列有序
+            merge(arr, left, mid, right, temp);//将两个有序子数组合并操作
+        }
+    }
+
+
+    /**
+     * 将两个有序的数组 合并
+     *
+     * @param arr
+     * @param left
+     * @param mid
+     * @param right
+     * @param temp
+     */
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;//左序列指针
+        int j = mid + 1;//右序列指针
+        int t = 0;//临时数组指针
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            } else {
+                temp[t++] = arr[j++];
+            }
+        }
+        while (i <= mid) {//将左边剩余元素填充进temp中
+            temp[t++] = arr[i++];
+        }
+        while (j <= right) {//将右序列剩余元素填充进temp中
+            temp[t++] = arr[j++];
+        }
+        t = 0;
+        //将temp中的元素全部拷贝到原数组中
+        while (left <= right) {
+            arr[left++] = temp[t++];
+        }
+    }
+
+
+    public static void quickSort(int a[],int left,int right)
+    {
+        int dp;
+        if(left<right)
+        {
+            dp=partition(a,left,right);
+            quickSort(a,left,dp-1);
+            quickSort(a,dp+1,right);
+        }
+    }
+
+    public static int partition(int a[],int left,int right)
+    {
+        int i=left;
+        int j=right;
+        int temp=a[i];
+        while(i<j)
+        {
+            while(i<j && a[j]>=temp)
+                j--;
+            if(i<j)
+                a[i]=a[j];
+            while(i<j && a[i]<=temp)
+                i++;
+            if(i<j)
+                a[j]=a[i];
+        }
+        a[i]=temp;
+        return i;
     }
 
     public static void main(String[] args) {
@@ -149,10 +287,8 @@ public class Sort {
 //        bubbleSort(arr);
 //        selectionSort(arr);
 //        insertionSort(arr);
-        shellSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-        }
+        quickSort(arr,0,arr.length-1);
+        Stream.of(arr).skip(1).forEach(System.out::print);
 
     }
 
